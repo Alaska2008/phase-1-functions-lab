5f7091d97a0fcd568792fe60738eaf7cca34a293
Amen Atikpo <alaskale2024@gmail.com>
2022-06-10 06:14:13 +0000

    Function la final

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(x){
-    if (x<0){
-        x = Math.abs(x)
-    }
-
-    return x-42
+    
+        return Math.abs(x-42)
 }
 
 function distanceFromHqInFeet(x){
    return distanceFromHqInBlocks(x)*264
 }
 function distanceTravelledInFeet(x,y){
-    y =Math.abs(y);
-    return (y-x)*264
+    
+    return Math.abs(y-x)*264
 
 }
 function calculatesFarePrice(x,y){
-    let feet =(distanceTravelledInFeet(x,y))-400
+    let feet =(distanceTravelledInFeet(x,y))
     if (feet <=400){
         return 0
     }
-    else if ((400<feet) & (feet>=2000)){
+    else if (400<feet && feet<=2000){
+        feet = feet-400
         return 0.02*feet
     }
-    else if ((2000<feet) & (feet>=2500)){
+    else if (2000<feet && feet<=2500){
+        feet= feet-400
          return 25
     }
     else {
-        return 'Cannot travel that far'
+        return 'cannot travel that far'
     }
        
 }
